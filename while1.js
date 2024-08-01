807ae5846e4f797d2ecea3b5b3f11f485df219d8
brkmrc35 <brcmrc1453@gmail.com>
2024-08-02 01:00:02 +0300

    while döngüsü

---
+++ while1.js
@@ -0,0 +1,10 @@
+let gizlisayi = Math.floor(Math.random() * 10);
+while(1 == 1){
+    let tahmin = prompt("SAYI SÖYLE");
+    if(tahmin == gizlisayi){
+        alert("BRAVO , DOĞRU TAHMİN");
+    }
+    else{
+        alert("YANLIŞ TAHMİN , TEKRAR DENE");
+    }
+}
